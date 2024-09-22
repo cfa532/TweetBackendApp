662848267dfd38acb53eec9b1e6c357954cfd7cb
cfa532 <cfa532@yahoo.com>
2024-09-22 20:41:29 +0800

    keystore generated.

--- upgrade.js
+++ upgrade.js
@@ -1,8 +1,24 @@
 ((request, args) => {
     try {
+        const APP_ID = request["aid"]       // App ID assigned by Leither upon publication
+        const APP_EXT = "com.example.twitterclone"
+        const APP_MARK = "package upgrade download"
+
+        // get mid of upgrade app package.
+        let authSid = lapi.BELoginAsAuthor()
+        let mid = lapi.MMCreate(authSid, APP_ID, APP_EXT, APP_MARK, 1, 0x07276704)
+        let appVersion = lapi.GetVar("", "mimeiversions", mid)
+        console.log("Package ver:", JSON.stringify(appVersion), mid)
+
+        //{
+        // "Versions":[{"Version":"1","MacRes":"QmRTT1L6pxpeyptApszu9LYzNn9sMgoJg6qgTpCe54gqGD","MacRef":"MqKW0iTyhcZ77pPDD4owkVfw2ql"}],
+        // "SpecialVers":[{"VerName":"last","Version":"1"}],
+        // "MinDifSeq":0
+        // }
         return {
-            appVersion: "1.0.0",
-            packageId: ""
+            version: appVersion.Versions[0].Version,
+            packageId: mid,
+            mission: "minor"     // App stop working without upgrade. minor, major, critical
         }
     } catch(e) {
         console.error(e)
