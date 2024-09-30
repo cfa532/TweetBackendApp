1710b08e37b6c4d109b3e64202f246926477972f
cfa532 <cfa532@yahoo.com>
2024-09-30 20:41:11 +0800

    Update get_tweets.js

--- get_tweets.js
+++ get_tweets.js
@@ -7,7 +7,7 @@
         let userId = request["userid"]
         let mmsid = lapi.MMOpen("", userId, "last")
     
-        console.log(startScore, endScore)
+        console.log("get_tweets", startScore, endScore)
         let arr = lapi.Zrangebyscore(mmsid, TWT_LIST_KEY, endScore, startScore, 0, 100)
         return arr.map(sp => {
             return lapi.RunMApp("get_tweet", {aid: request["aid"], ver:"last", userid: userId, tweetid: sp.Member}, [])
