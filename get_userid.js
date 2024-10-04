1b249b0db711246f9d767b6884a8b8d9397e51ed
cfa532 <cfa532@yahoo.com>
2024-10-04 16:49:34 +0800

    Update get_userid.js

--- get_userid.js
+++ get_userid.js
@@ -1,10 +1,10 @@
 ((request, args)=>{
     const APP_ID = request["aid"]       // App ID assigned by Leither upon publication
     const APP_EXT = "com.example.twitterclone"
-    let APP_MARK = "στηναρχή"
-
-    APP_MARK = request["phrase"]
+    let APP_MARK = request["phrase"]
+    console.log("get userid login:", APP_ID, APP_MARK)
     let authSid = lapi.BELoginAsAuthor()
+    console.log("authsid", authSid)
     let userId = lapi.MMCreate(authSid, APP_ID, APP_EXT, APP_MARK, 2, 0x07276704)
     console.log("Get userid=", userId)
 
