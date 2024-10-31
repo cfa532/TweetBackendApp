3ccda105f0366d91d40c2a1e1faa7fea14d36adb
cfa532 <cfa532@yahoo.com>
2024-10-31 19:06:34 +0800

    Update delete_tweet.js

--- delete_tweet.js
+++ delete_tweet.js
@@ -1,20 +1,27 @@
 ((request, args)=>{
     try {
         const TWT_LIST_KEY = "list_of_tweets_mid"
+        const TWT_CONTENT_KEY = "core_data_of_tweet"
 
         let tweetId = request["tweetid"]    // tweet Id to be removed
         let authorMid = request["authorid"]
+        let authSid = lapi.BELoginAsAuthor()
 
-        const authSid = lapi.BELoginAsAuthor()
-        let mmsid = lapi.MMOpen(authSid, authorMid, "cur")
+        // if there is attachments, delete its reference too.
+        let mmsid = lapi.MMOpen(authSid, tweetId, "last")
+        let tweet = lapi.Get(mmsid, TWT_CONTENT_KEY)
+        tweet.attachments?.forEach(element => {
+            lapi.MMDelRef(authSid, tweetId, element.mid)
+        });
+
+        mmsid = lapi.MMOpen(authSid, authorMid, "cur")
         lapi.Zrem(mmsid, TWT_LIST_KEY, tweetId)
         lapi.MMBackup(authSid, authorMid, "", "delref=true")
         lapi.MMDelRef(authSid, authorMid, tweetId)
 
-        mmsid = lapi.MMOpen(authSid, tweetId, "cur")
         lapi.MMDelVers(mmsid, tweetId)
         lapi.MMBackup(authSid, tweetId, "", "delref=true")
-        console.log("Delete tweet ", tweetId)
+        console.log("Delete tweet ", JSON.stringify(tweet))
         return tweetId
         // lapi.MiMeiPublish(authSid, "", authorMid)
     } catch(e) {
