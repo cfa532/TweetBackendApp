c05abe4ce0b816bb7555f82426be3142d28bf32b
cfa532 <cfa532@yahoo.com>
2024-11-18 19:31:51 +0800

    Update get_provider.js

--- get_provider.js
+++ get_provider.js
@@ -10,9 +10,7 @@
         let ips = []
         providers.forEach(element => {
             // iterate providers
-            console.log("element", JSON.stringify(element))
             ips.concat(element.forEach(element2 => {
-                console.log("element2", JSON.stringify(element2))
                 // iterate IP addresses of a provider, to find the best one.
                 // element2 format [183.156.208.29:1088, 3080507421]
                 // if (element2[1] < mini || mini == null) {
