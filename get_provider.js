527c6a894e4126935072e19035952cf957283bf6
cfa532 <cfa532@yahoo.com>
2024-11-18 15:01:14 +0800

    get provider IPs

--- get_provider.js
+++ get_provider.js
@@ -1,25 +1,29 @@
 ((request, args)=>{
     try {
-        // request, lapi are global variables
+        // Used only by tweet web.
         let mid = request["mid"]
         let providers = lapi.GetVar("", "mmprovsips", mid)
         console.log("providers", providers)
         providers = JSON.parse(providers)
         let mini = null
         let ip = ""
+        let ips = []
         providers.forEach(element => {
             // iterate providers
-            element.forEach(element2 => {
+            console.log("element", JSON.stringify(element))
+            ips.concat(element.forEach(element2 => {
+                console.log("element2", JSON.stringify(element2))
                 // iterate IP addresses of a provider, to find the best one.
                 // element2 format [183.156.208.29:1088, 3080507421]
-                if (element2[1] < mini || mini == null) {
-                    mini = element2[1]
-                    ip = element2[0]
-                }   
-            });
+                // if (element2[1] < mini || mini == null) {
+                //     mini = element2[1]
+                //     ip = element2[0]
+                // }
+                ips.push(element2[0])
+            }))
         });
-        console.log("Best provider:", ip, mid)
-        return ip
+        console.log("IP addresses for", mid, JSON.stringify(ips))
+        return ips
     } catch(e) {
         console.error("Error get_provider:", JSON.stringify(request), e)
     }
