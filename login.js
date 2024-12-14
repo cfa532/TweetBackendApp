21ae48654ced76b7d5a55d25985c30634560a5c8
cfa532 <cfa532@yahoo.com>
2024-12-14 14:48:49 +0800

    Update login.js

--- login.js
+++ login.js
@@ -12,7 +12,7 @@
             console.log("User does not exist.", request["username"])
             return {status: "failure", reason: "User does not exist"}
         }
-        console.log("open user", JSON.stringify(user))
+        console.log("Check login user", JSON.stringify(user))
         // need to check hashed password
         if (user.password == lapi.MMCreate(authSid, APP_ID, APP_EXT, request["password"], 1, 0x07276704)) {
             delete user.password
