764adc7787efb0bdaad1851b28db602097505fb9
cfa532 <cfa532@yahoo.com>
2024-12-30 12:01:57 +0800

    Update upload_ipfs.js

--- upload_ipfs.js
+++ upload_ipfs.js
@@ -5,16 +5,18 @@
     try {
         let authSid = lapi.BELoginAsAuthor();
         let fsid = request["fsid"]? request["fsid"] : lapi.MFOpenTempFile(authSid);
-        console.log("finished=", request["finished"], request["referenceid"])
         if (request["finished"] == "true") {
-            if (request["referenceid"] == undefined)
+            if (request["referenceid"] == undefined) {
+                // no ref to add, this is an attachment of a tweet.
+                // It will be add as ref to the tweetId later.
                 return lapi.MFTemp2Ipfs(fsid, null)
+            }
+            // add new ipfs as ref to a parent Mimei, usually an userId
             return lapi.MFTemp2Ipfs(fsid, request["referenceid"])
         }
         let offset = parseInt(request["offset"], 10)
         let b = new Uint8Array(args[0])         // key point.
         let count = lapi.MFSetData(fsid, b, offset);
-        console.log("count=", count, offset)
         return fsid;
     } catch(e) {
         console.error("Error upload_ipfs:", JSON.stringify(request), e)
