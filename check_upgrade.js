d07780db0c168fba9bce217168d1d514fb655658
cfa532 <cfa532@yahoo.com>
2025-01-16 22:17:42 +0800

    Update check_upgrade.js

--- check_upgrade.js
+++ check_upgrade.js
@@ -7,8 +7,7 @@
         // get mid of upgrade app package.
         let authSid = lapi.BELoginAsAuthor()
         let mid = lapi.MMCreate(authSid, APP_ID, APP_EXT, APP_MARK, 1, 0x07276704)
-        let appVersion = lapi.GetVar("", "mimeiversions", mid)
-        console.log("Package ver:", JSON.stringify(appVersion), mid)
+        // let appVersion = lapi.GetVar("", "mimeiversions", mid)
 
         /**
          * http://tweet1.sharefire.store/upload
@@ -16,13 +15,15 @@
          * 9OCLYP-SXzen3e171-Ei_6N3Gwl installation package mimei.
          * command line tool: ./publish_upgrade.sh app-release.apk
          */
-        return {
+        let ret = {
             // version: appVersion.Versions[appVersion.Versions.length-1].Version,
             version: 25,  // set it larger than defaultConfig.versionName in build.gradle to force upgrade.
             packageId: mid,
             mission: "minor",     // App stop working without upgrade. minor, major, critical. Not used.
             domain: "t1.fireshare.store",   // base url to be used for deeplink and share.
         }
+        console.log("check_upgrade", request["uid"], JSON.stringify(ret))
+        return ret
     } catch(e) {
         console.error("Error check_upgrade", JSON.stringify(request), e)
     }
