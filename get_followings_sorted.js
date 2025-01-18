ba954aa2566f3594086cac515abecef5851e1f0f
cfa532 <cfa532@yahoo.com>
2025-01-18 13:24:18 +0800

    Update get_followings_sorted.js

--- get_followings_sorted.js
+++ get_followings_sorted.js
@@ -1,7 +1,6 @@
 ((request, args)=>{
     try {
         const FOLLOWINGS_LIST = "list_of_followings_mid"
-
         let userId = request["userid"]
         let mmsid = lapi.MMOpen("", userId, "last")
         return lapi.Hgetall(mmsid, FOLLOWINGS_LIST)
