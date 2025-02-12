7ea15c3bdf694b71b45c5b94c69d40cddef91a52
cfa532 <cfa532@yahoo.com>
2025-02-12 19:51:02 +0800

    Update get_top_tweets.js

--- get_top_tweets.js
+++ get_top_tweets.js
@@ -5,9 +5,9 @@
          */
         const TOP_TWEETS = "top_tweet_list"
         let mmsid = lapi.MMOpen("", request["userid"], "last")
-        return lapi.Hkeys(mmsid, TOP_TWEETS).map(element => {
-            let ts = lapi.Hget(mmsid, TOP_TWEETS, element).toString()
-            return {tweetId: element, timestamp: ts}
+        return lapi.Hkeys(mmsid, TOP_TWEETS).map(tid => {
+            let ts = lapi.Hget(mmsid, TOP_TWEETS, tid).toString()
+            return {tweetId: tid, timestamp: ts}
         });
     } catch(e) {
         console.error("Error get_top_tweets:", JSON.stringify(request), e)
