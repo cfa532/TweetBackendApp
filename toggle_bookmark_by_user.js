e87d0ce05fb081abc6dbc5ad0fd11ea05b3204ea
cfa532 <cfa532@yahoo.com>
2025-03-11 16:05:14 +0800

    provide tweet ok

--- toggle_bookmark_by_user.js
+++ toggle_bookmark_by_user.js
@@ -4,52 +4,58 @@
  */
 ((request, args)=>{
     const OWNER_DATA_KEY = "data_of_author"
-    const BOOKMARK_LIST = "bookmark_list"
     const APP_ID = request["aid"]
     const userId = request["userid"]
-    const user = getUser(userId)
-    const tweetId = request["tweetid"]  // tweetID that appUser bookmarked or favored
-    const isBookmarked = request["isbookmarked"]
-
-    const nodeId = lapi.GetVar("", "hostid")    // current node id
-    console.log("Toggle bookmark by user", nodeId, tweetId, userId, isBookmarked, user.hostIds[0])
-
-    if (user.hostIds?.findIndex(id => id == nodeId) != 0) {
-        const systemSid = lapi.BEOpenAppDataNode("cur", APP_ID)
-        const userData = lapi.RunMApp("toggle_bookmark_by_user",
-            { aid: APP_ID, ver: "last",
-                nid: hostId, sid: systemSid,
-                userid: userId, mid: tweetId }, []
-        )
-        console.log("toggle_bookmark_by_user remote ret=", JSON.stringify(userData))
-
-        // user local data will be updated by Leither
-        return userData
-    } else {
-        console.log("Before toggleBookmark by user")
-        toggleBookmarkByUser(tweetId, userId, isBookmarked)
-        // const authSid = lapi.BELoginAsAuthor();
-        // if (isBookmark) {
-        //     lapi.MiMeiSync(authSid, "", tweetId, {})
-        //     lapi.MiMeiProvide(authSid, "", tweetId)
-        // } else {
-        //     lapi.MiMeiUnprovide(authSid, "", tweetId)
-        //     lapi.MMDelVers(authSid, tweetId)
-        // }
-        // console.log("After toggleBookmark by user")
+    try {
+        const user = getUser(userId)
+        const tweetId = request["tweetid"]  // tweetID that appUser bookmarked or favored
+        const isBookmarked = request["isbookmarked"]
+    
+        const nodeId = lapi.GetVar("", "hostid")    // current node id
+        console.log("Toggle bookmark by user", nodeId, tweetId, userId, isBookmarked, user.hostIds[0])
+    
+        if (user.hostIds?.findIndex(id => id == nodeId) != 0) {
+            const systemSid = lapi.BEOpenAppDataNode("cur", APP_ID)
+            const userData = lapi.RunMApp("toggle_bookmark_by_user",
+                { aid: APP_ID, ver: "last",
+                    nid: hostId, sid: systemSid,
+                    userid: userId, mid: tweetId }, []
+            )
+            console.log("toggle_bookmark_by_user remote ret=", JSON.stringify(userData))
+    
+            // user local data will be updated by Leither
+            return userData
+        } else {
+            console.log("Before toggleBookmark by user")
+            toggleBookmarkByUser(tweetId, userId, isBookmarked)
+            const authSid = lapi.BELoginAsAuthor();
+            if (isBookmarked) {
+                lapi.MiMeiSync(authSid, "", tweetId, {})
+                lapi.MiMeiProvide(authSid, "", tweetId)
+            } else {
+                lapi.MiMeiUnprovide(authSid, "", tweetId)
+                lapi.MMDelVers(authSid, tweetId)
+            }
+            console.log("After toggleBookmark by user")
+            const userSid = lapi.MMOpen("", userId, "last")
+            const userData = lapi.Get(userSid, OWNER_DATA_KEY)
+            console.log("toggle_bookmark_by_user ret=", JSON.stringify(userData))
+            return userData
+        }
+    } catch(e) {
+        console.error("toggle user bookmark error", e)
         const userSid = lapi.MMOpen("", userId, "last")
-        const userData = lapi.Get(userSid, OWNER_DATA_KEY)
-        console.log("toggle_bookmark_by_user ret=", JSON.stringify(userData))
-        return userData
+        return lapi.Get(userSid, OWNER_DATA_KEY)
     }
 
     function toggleBookmarkByUser(
-        tweetId, userId, isBookmark
+        tweetId, userId, isBookmarked
     ) {
         try {
+            const BOOKMARK_LIST = "bookmark_list"
             const authSid = lapi.BELoginAsAuthor()
             const userSid = lapi.MMOpen(authSid, userId, "cur")
-            if (isBookmark) {
+            if (isBookmarked) {
                 lapi.Hset(userSid, BOOKMARK_LIST, tweetId, Date.now())
             } 
             else {
--- toggle_favorite_by_user.js
+++ toggle_favorite_by_user.js
@@ -2,51 +2,58 @@
  * Update the user's favorite list by adding or removing the tweetId.
  * @isFavorite indicates if the tweetId should be added to favorite list or not.
  */
+
 ((request, args)=>{
     const OWNER_DATA_KEY = "data_of_author"
-    const FAVORITE_LIST = "favorite_list"
     const APP_ID = request["aid"]
     const userId = request["userid"]
-    const user = getUser(userId)
-    const tweetId = request["tweetid"]  // tweetID that appUser bookmarked or favored
-    const isFavorite = request["isfavorite"]
-
-    const nodeId = lapi.GetVar("", "hostid")    // current node id
-    console.log("Toggle favorite by user", nodeId, tweetId, userId, isFavorite, user.hostIds[0])
-
-    if (user.hostIds?.findIndex(id => id == nodeId) != 0) {
-        const systemSid = lapi.BEOpenAppDataNode("cur", APP_ID)
-        const userData = lapi.RunMApp("toggle_favorite_by_user",
-            { aid: APP_ID, ver: "last",
-                nid: hostId, sid: systemSid,
-                userid: userId, mid: tweetId }, []
-        )
-        console.log("toggle_favorite_by_user remote ret=", JSON.stringify(userData))
-
-        // user local data will be updated by Leither
-        return userData
-    } else {
-        console.log("Before toggleFavorite by user")
-        toggleFavoriteByUser(tweetId, userId, isFavorite)
-        // const authSid = lapi.BELoginAsAuthor();
-        // if (isFavorite) {
-        //     lapi.MiMeiSync(authSid, "", tweetId, {})
-        //     lapi.MiMeiProvide(authSid, "", tweetId)
-        // } else {
-        //     lapi.MiMeiUnprovide(authSid, "", tweetId)
-        //     lapi.MMDelVers(authSid, tweetId)
-        // }
-        // console.log("After toggleFavorite by user")
+    try {
+        const user = getUser(userId)
+        const tweetId = request["tweetid"]  // tweetID that appUser bookmarked or favored
+        const isFavorite = request["isfavorite"]
+    
+        const nodeId = lapi.GetVar("", "hostid")    // current node id
+        console.log("Toggle favorite by user", nodeId, tweetId, userId, isFavorite, user.hostIds[0])
+    
+        if (user.hostIds?.findIndex(id => id == nodeId) != 0) {
+            const systemSid = lapi.BEOpenAppDataNode("cur", APP_ID)
+            const userData = lapi.RunMApp("toggle_favorite_by_user",
+                { aid: APP_ID, ver: "last",
+                    nid: hostId, sid: systemSid,
+                    userid: userId, mid: tweetId }, []
+            )
+            console.log("toggle_favorite_by_user remote ret=", JSON.stringify(userData))
+    
+            // user local data will be updated by Leither
+            return userData
+        } else {
+            console.log("Before toggleFavorite by user")
+            toggleFavoriteByUser(tweetId, userId, isFavorite)
+            const authSid = lapi.BELoginAsAuthor();
+            if (isFavorite) {
+                lapi.MiMeiSync(authSid, "", tweetId, {})
+                lapi.MiMeiProvide(authSid, "", tweetId)
+            } else {
+                lapi.MiMeiUnprovide(authSid, "", tweetId)
+                lapi.MMDelVers(authSid, tweetId)
+            }
+            console.log("After toggleFavorite by user")
+            const userSid = lapi.MMOpen("", userId, "last")
+            const userData = lapi.Get(userSid, OWNER_DATA_KEY)
+            console.log("toggle_favorite_by_user ret=", JSON.stringify(userData))
+            return userData
+        }
+    } catch(e) {
+        console.error("toggle user favorite error", e)
         const userSid = lapi.MMOpen("", userId, "last")
-        const userData = lapi.Get(userSid, OWNER_DATA_KEY)
-        console.log("toggle_favorite_by_user ret=", JSON.stringify(userData))
-        return userData
+        return lapi.Get(userSid, OWNER_DATA_KEY)
     }
 
     function toggleFavoriteByUser(
         tweetId, userId, isFavorite
     ) {
         try {
+            const FAVORITE_LIST = "favorite_list"
             const authSid = lapi.BELoginAsAuthor()
             const userSid = lapi.MMOpen(authSid, userId, "cur")
             if (isFavorite) {
