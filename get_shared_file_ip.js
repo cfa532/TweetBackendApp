d20a98f8480e9de5240714d47b37c564b6529502
cfa532 <cfa532@yahoo.com>
2025-04-06 17:35:15 +0800

    Update get_shared_file_ip.js

--- get_shared_file_ip.js
+++ get_shared_file_ip.js
@@ -3,9 +3,7 @@
  */
 ((request, args)=>{
     try {
-        console.log("get_shared_file_ip", JSON.stringify(request))
         const ips = lapi.GetVar("", "mmprovsips", request["mid"])
-        console.log("ips", ips)
         const providers = JSON.parse(ips)
         let ip = ""
         let mini = null
