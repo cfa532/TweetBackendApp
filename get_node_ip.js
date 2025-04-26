8b9cd91aaba510cddd951eb0527022ec05608a34
cfa532 <cfa532@yahoo.com>
2025-04-26 11:52:51 +0800

    Update get_node_ip.js

--- get_node_ip.js
+++ get_node_ip.js
@@ -4,7 +4,6 @@
 ((request, args)=>{
     try {
         let ips = lapi.GetVar("", "ips", request["nodeid"])
-        console.log("get_node_ip", request["nodeid"], ips)
         return ips
     } catch(e) {
         console.error("Error get_node_id", JSON.stringify(request), e)
