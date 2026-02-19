1f6a848a3b2ad679fe64374b92c92340b33e6811
cfa532 <cfa532@yahoo.com>
2026-02-19 19:04:59 +0800

    48

--- check_upgrade.js
+++ check_upgrade.js
@@ -66,7 +66,7 @@
          */
         let ret = {
             // Dynamic version from package: appVersion.Versions[appVersion.Versions.length-1].Version
-            version: 47,  // Set larger than defaultConfig.versionName in build.gradle to force upgrade
+            version: 48,  // Set larger than defaultConfig.versionName in build.gradle to force upgrade
             packageId: mid,  // Must match the mid of installation package created by upload_package.js
             mission: "minor",  // App stops working without upgrade (minor, major, critical)
             domain: "t1.blockvault.store",  // Base URL for deeplinks and sharing
