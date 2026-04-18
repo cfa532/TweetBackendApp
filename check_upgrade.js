98f8b36d7b7b3f43a0fa9a81ce5977d4012d1011
cfa532 <cfa532@yahoo.com>
2026-04-18 11:25:41 +0800

    55

--- check_upgrade.js
+++ check_upgrade.js
@@ -66,7 +66,7 @@
          */
         let ret = {
             // Dynamic version from package: appVersion.Versions[appVersion.Versions.length-1].Version
-            version: 53,  // Set larger than defaultConfig.versionName in build.gradle to force upgrade
+            version: 55,  // Set larger than defaultConfig.versionName in build.gradle to force upgrade
             packageId: mid,  // Must match the mid of installation package created by upload_package.js
             mission: "minor",  // App stops working without upgrade (minor, major, critical)
             domain: "t1.www333.store",  // Base URL for deeplinks and sharing
