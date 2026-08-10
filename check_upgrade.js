469e0f073d351ed2bca2434fa5651b18b7842260
cfa532 <cfa532@yahoo.com>
2026-08-10 15:21:51 +0800

    Update check_upgrade.js

--- check_upgrade.js
+++ check_upgrade.js
@@ -74,7 +74,7 @@
             version: 72,  // Set larger than defaultConfig.versionName in build.gradle to force upgrade
             packageId: mid,  // Must match the mid of installation package created by upload_package.js
             mission: "minor",  // App stops working without upgrade (minor, major, critical)
-            domain: "t1.www3.shop",  // Base URL for deeplinks and sharing
+            domain: "t1.w3w3.store",  // Base URL for deeplinks and sharing
         }
         
         lapi.Debug("Tweed check_upgrade: %s", JSON.stringify(ret))
